f5f4a8abe944034a2ff637f0b532818c535a1b75
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-10 21:06:12 +1000

    began navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,18 @@
+import './Nav.css'
+
+import GitHubIcon from './icon-github.png';
+import LinkedInIcon from './icon-linkedin.gif';
+import MailIcon from './icon-mail.png';
 
 function Nav() {
     return (
         <div>
             <ul>
-                <li>GitHub</li>
-                <li>LinkedIn</li>
-                <li>Email</li>
+                <li><a href='https://github.com/johnnymadigan'><img src={GitHubIcon} alt='GitHub'/>GitHub</a></li>
+                <li><a href='https://www.linkedin.com/in/johnnymadigan/'><img src={LinkedInIcon} alt='LinkedIn'/>LinkedIn</a></li>
+                <li><a href='mailto:johnny.madigan@icloud.com'><img src={MailIcon} alt='Email'/>Email</a></li>
             </ul>
+            <h2>johnny madigan</h2>
         </div>
     );
 }
