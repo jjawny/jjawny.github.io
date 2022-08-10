2c2fc98f8b0dda95543d90131bf8ebef55365a7a
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-10 20:07:57 +1000

    began hero

---
+++ src/Hero.js
@@ -0,0 +1,15 @@
+import './Hero.css';
+
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className='hero-content'>
+                <h2>hi i'm...</h2>
+                <h1>johnny madigan</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+
+function Nav() {
+    return (
+        <div>
+            <ul>
+                <li>GitHub</li>
+                <li>LinkedIn</li>
+                <li>Email</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
