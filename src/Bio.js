45db9c4c8640a1064650aa8a80d11cf73b6c1bf3
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-16 18:48:24 +1000

    added bio component

--- src/Bio.js
+++
@@ -1,9 +0,0 @@
-import './Bio.css'
-
-function Bio() {
-    return (
-        <div className="bio-container">bio</div>
-    );
-}
-
-export default Bio;
---
+++ src/Bio/Bio.js
@@ -0,0 +1,18 @@
+import './Bio.css'
+
+function Bio() {
+    return (
+        <div className="bio-container">
+            <h2>who am i ?</h2>
+            <p>
+                I have recently graduated with distinction from <em>Queensland University of Technology</em> with a bachelor's degree in Computer Science and now currently working as an IT Support Analyst at <em>Griffith University</em>.
+                <br/><br/>
+                What I value most is working within diverse teams, as collaborating with different perspectives leads to creating great products. 
+                <br/><br/>
+                With my wealth of experience supporting clients, my continuous learning attitude, and my creative problem-solving skills, I believe I can bring value to any team.
+            </p>
+        </div>
+    );
+}
+
+export default Bio;
