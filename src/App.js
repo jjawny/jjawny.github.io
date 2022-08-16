3b806bc23fc205588df564ee07993d1f909e5fc3
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-16 20:03:05 +1000

    refactored app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Hero from './Hero';
-import HeaderNav from './HeaderNav';
-import Bio from './Bio';
-import Accessability from './Accessability';
-import Slide from './Slide';
+
+import Nav from './Navi/Navi';
+import Hero from './Hero/Hero';
+import Bio from './Bio/Bio';
+import Projects from './Projects/Projects';
+import Footer from './Footer/Footer';
 
 
 function App() {
@@ -18,23 +18,16 @@ function App() {
    * This data is a local repo file that comprises of project info from 
    * pure strings for descriptions etc, to file paths for images.
    * 
-   * This method will be called as a side-effect to each time the DOM
-   * is re-rendered using the 'useEffect' hook.
+   * This function will be called as a side-effect upon rendering/mounting 
+   * to the DOM for the first time using the 'useEffect' hook.
    * 
-   * The 'useState' hook will store this data and render it to the DOM
+   * The 'useState' hook will remember/store this data and render it to the DOM
    * within components.
-   * 
-   * NESTED FUNCTION in order to be within the same
-   * scope as the React hooks (useState, useEffect, etc)
    */
 
-  /* {{{{{ USESTATE HOOK }}}}}
-  For storing/updating project JSON data for components to use.
-  The data will be passed into Home & Project page components for use.
-  [current state (data), function to update state (assign updated data)] */
   const [data, setData] = useState([]); // default is empty array
   
-   const getData = () => {
+  const getData = () => {
     fetch('projects.json', {
       headers: {
         /* headers to let client know that we
@@ -43,18 +36,9 @@ function App() {
         'Accept': 'application/json'
       }
     })
-    /* promise to show fetch RESPONSE */
-    .then((response) => (response.json()))
-    /* promise to update STATE with the new data */
-    .then((myJSON) => (setData(myJSON)));
+    .then((response) => (response.json())) /* promise to get response as JSON */
+    .then((myJSON) => (setData(myJSON))); /* promise to update STATE with the JSON data */
   }
-
-  
-
-  /* {{{{{ USEEFFECT HOOK }}}}}
-  For side-effects (fetching is a side-effect). 
-  Hook's purpose is to execute side-effect code upon every render,
-  allowing us to fetch the most up-to-date data from our JSON */
 
   /* perform once upon first render */
   useEffect(() => {
@@ -65,11 +49,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HeaderNav color="light" fixed="top" expand="md"/>
+        <Nav color="light" fixed="top" expand="md"/>
         <Hero/>
         <Bio/>
-        <Slide data={data}/>
-        <Accessability/>
+        <Projects projects={data}/>
+        <Footer/>
       </header>
     </div>
   );
