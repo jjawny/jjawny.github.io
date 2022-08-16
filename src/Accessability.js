0fd5008ff2614555443868e11c1f3e43551ca477
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-16 18:48:45 +1000

    added footer component

--- src/Accessability.js
+++
@@ -1,9 +0,0 @@
-import './Accessability.css'
-
-function Accessability() {
-    return (
-        <div className="access-container">text</div>
-    );
-}
-
-export default Accessability;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>
+                Copyright &copy; Johnny Madigan. All rights reserved.<br/>
+                <a href="https://johnnymadigan.github.io/v1">V1</a>&nbsp;
+                <a href="https://johnnymadigan.github.io/v2">V2</a><br/><br/>
+                <img src="./favicon.ico" alt='logo'/>
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
