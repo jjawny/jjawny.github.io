73bbc3e53ad6cb175ecea6f7ebf9bf2f86f6c29f
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-25 20:28:20 +1000

    refactored method for sticky with no overflow on parent
    contain: paint prevents overflow like overflow: hidden but allows sticky to function properly

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -23,19 +23,10 @@ function Projects(props) {
         setSide(currentSide => !currentSide);
     };
 
-    // IMPORTANT NOTE 2 SELF: sticky property + overflow hidden = does not work
-    // The sliding affect requires 2 divs to be translated in/out of the parent container, causing...
-    // unwanted whitespace on right-side of the entire site (since the divs stretch the container horizontally)
-    // this is expected behavior and requires overflow to be hidden
-    //
-    // Because we want the right-side (current highlighted project) to be fixed on screen for a better UX...
-    // we must toggle the overflow style in-line (see below) to visible for the right-side and hidden-for the left
-    // as only when viewing the left-side do we see the overflow, therefore it is not needed on the right-side at all
-
-    // Projects titles are mapped as a list of buttons (LEFT), with a event listener that changes the current highlighted project
-    // The project that is currently highlighted is displayed (RIGHT), with another inner map for each shield/badge
+    // Projects titles are mapped as a list of buttons (LEFT), with a event listener that changes the current highlighted project.
+    // The project that is currently highlighted is displayed (RIGHT), with another inner map for each shield/badge.
     return (
-        <div className="projects-container" style={{overflow: `${side? 'visible' : 'hidden'}`}}>
+        <div className="projects-container" >
             <div className={`both-sides left-side ${side? 'slideLeft' : ''}`}>
                 {props.projects.map((p) => (<button key={p.title} onClick={() => selectHighlight(p)}>{p.title}</button>))}
             </div>
