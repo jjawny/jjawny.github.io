ac72a217ad6f58df51e989b2cd3457078da883b1
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-08-29 21:30:49 +1000

    alternative way of fixing overflow

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -6,7 +6,7 @@ import Project from './Project';
 import './Projects.css';
 
 function Projects(props) {
-    // sides: true = show left, false = show right
+    // side in view: false = LEFT, true = RIGHT
     const [ side , setSide ] = useState(false);
 
     // last selected project to highlight
@@ -28,14 +28,18 @@ function Projects(props) {
         setSide(currentSide => !currentSide);
     };
 
-    // Projects titles are mapped as a list of buttons (LEFT), with a event listener that changes the current highlighted project.
-    // The project that is currently highlighted is displayed (RIGHT), with another inner map for each shield/badge.
+    /* Projects titles are mapped as a list of buttons (LEFT), with a event listener that changes the current highlighted project.
+    The project that is currently highlighted is displayed (RIGHT), with another inner map for each shield/badge. */
+
+    /* Project-container overflows on R-side (only when R-side off-screen)...
+    Set to hidden only when R-side off-screen to remove extra margin...
+    Set to nothing to allow sticky to work when L-side off-screen */
     return (
-        <div className="projects-container" >
-            <div className={`both-sides left-side ${side? 'slideLeft' : ''}`}>
+        <div className="projects-container" style={side ? {overflow: ''} : {overflow: 'hidden'}}>
+            <div className={`both-sides left-side ${side ? 'slideLeft' : ''}`}>
                 {props.projects.map((p) => (<button key={p.title} onClick={() => selectHighlight(p)}>{p.title}</button>))}
             </div>
-            <div className={`both-sides right-side ${!side? 'slideRight' : ''}`}>
+            <div className={`both-sides right-side ${!side ? 'slideRight' : ''}`}>
                 <button style={{width: "100%", textAlign: "right"}} onClick={() => selectHighlight(highlight)}><img src={iBack} alt='Go back' className='back-button'/></button>
                 <Project {...highlight} loaded={loaded} setLoaded={setLoaded}/>
             </div>
