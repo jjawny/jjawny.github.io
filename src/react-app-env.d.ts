86cf38d4dc3a8543b6696ae3b1fa05453bd8e3b3
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-09-04 19:51:49 +1000

    file to allow static image imports in TSX files

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,8 @@
+/* Needed to allow imports of static files in components, 
+see: https://stackoverflow.com/a/71100012/19568276 */
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "*.gif";
