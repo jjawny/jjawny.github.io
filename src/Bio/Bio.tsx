a779863f98c9dfab7b11131b908ec30c2a6f0b36
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-10-25 21:26:41 +1000

    Updated bio text

--- src/Bio/Bio.tsx
+++ src/Bio/Bio.tsx
@@ -6,9 +6,9 @@ function Bio() {
         <div className="bio-container">
             <h2>who am i ?</h2>
             <p>
-                Graduate Developer at eHealth, completed a bachelor’s degree in Computer Science at QUT, with 6 years experience at Griffith University in various support roles.
+                I’m a Software Engineer with a bachelor’s degree in Computer Science, currently working at eHealth Queensland on a variety of enterprise apps.
                 <br/><br/>
-                With my wealth of experience supporting clients, my continuous learning-attitude, and my creative problem-solving skills, I believe I can bring value to any team.
+                With my creative problem-solving skills and continuous learning-attitude, I believe I can bring value to any team.
             </p>
         </div>
     );
