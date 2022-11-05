8446fdffafc55791a6e328ded913fbd94d54011c
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-11-05 16:08:11 +1000

    Updated footer for consistent style

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -4,11 +4,17 @@ import './Footer.css'
 function Footer() {
     return (
         <div className='footer'>
+            <div className='logoTopMidWrapperName'>
+            <div className='logoTopWrapperName'>
+                <div className='logoTopName'></div>
+            </div>
+            <div className='logoMidName'></div>
+        </div>
+        <div className='logoBottomName'></div>
             <p>
                 Copyright &copy; Johnny Madigan. All rights reserved.<br/>
                 <a href="https://johnnymadigan.github.io/v1">V1</a>&nbsp;
                 <a href="https://johnnymadigan.github.io/v2">V2</a><br/><br/>
-                <img src="./favicon.ico" alt='logo'/>
             </p>
         </div>
     );
