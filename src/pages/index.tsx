bb3e92575cdd4534524d3a442155ef7c8397c662
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-03-19 10:32:18 +1000

    [ui] tweaked design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,47 +16,66 @@ const Home: NextPage = (props) => {
       </Head>
       <main className="min-h-[100vh] bg-gradient-to-r from-[#BFC8DB] to-[#96AAC5]">
         <div className="grid h-full w-screen select-none place-items-center">
-          <div className="flex w-screen flex-col py-20">
-            <h1 className="text-center text-8xl font-extrabold tracking-tighter text-white sm:text-[19vw]">
-              johnny madigan
-            </h1>
-            <div className="h-60">
-              <Canvas camera={{ position: [0, 0, 0] }}>
-                <Suspense fallback={null}>
-                  <Lighting />
-                  <Model />
-                </Suspense>
-              </Canvas>
-            </div>
-            <div className="flex flex-row justify-center pb-20">
-              <Link href={"https://github.com/johnnymadigan"}>
-                <Image
-                  src={"/github.png"}
-                  alt="github"
-                  height={50}
-                  width={50}
-                  className="duration-700 hover:scale-110"
-                />
-              </Link>
-              <Link href={"https://www.linkedin.com/in/johnnymadigan/"}>
-                <Image
-                  src={"/linkedin.png"}
-                  alt="linkedin"
-                  height={50}
-                  width={50}
-                  className="duration-700 hover:scale-110"
-                />
-              </Link>
+          <div className="flex w-screen flex-col pt-20">
+            <div className="h-screen">
+              <h1 className="self-center text-center text-8xl font-extrabold tracking-tighter text-white sm:text-[13vw]">
+                johnny madigan
+              </h1>
+              <div className="h-60">
+                <Canvas camera={{ position: [0, 0, 0] }}>
+                  <Suspense fallback={null}>
+                    <Lighting />
+                    <Model />
+                  </Suspense>
+                </Canvas>
+              </div>
+              <div className="flex flex-row justify-center">
+                <Link href={"https://github.com/johnnymadigan"}>
+                  <Image
+                    src={"/github.png"}
+                    alt="github"
+                    height={50}
+                    width={50}
+                    className="duration-700 hover:scale-110"
+                  />
+                </Link>
+                <Link href={"https://www.linkedin.com/in/johnnymadigan/"}>
+                  <Image
+                    src={"/linkedin.png"}
+                    alt="linkedin"
+                    height={50}
+                    width={50}
+                    className="duration-700 hover:scale-110"
+                  />
+                </Link>
+              </div>
             </div>
             <h1 className="text-center text-5xl font-extrabold tracking-tighter text-white">
               who am i?
             </h1>
-            <p className="max-w-2xl self-center text-center text-lg text-white">
-              I'm a Full Stack Software Developer currently working at
-              Queensland Health on a portfolio of enterprise apps. As someone
-              who thrives on learning, building, and finding solutions to
-              complex problems, I believe I can bring value to any team.
+            <p className="text-md max-w-4xl self-center px-10 text-center text-white">
+              <br />
+              I'm a{" "}
+              <span className="font-semibold text-amber-300">
+                Full Stack Software Developer
+              </span>{" "}
+              currently working at Queensland Health on a portfolio of
+              enterprise apps.
+              <br />
+              <br />
+              As someone who{" "}
+              <span className="font-semibold text-amber-300">thrives</span> on
+              learning, building, and finding solutions to complex problems, I
+              believe I can bring value to any team.
             </p>
+            <div className="mt-20 h-40 w-full bg-black py-10">
+              <div className="w-full text-center text-xs text-gray-500">
+                <a href="https://skfb.ly/6FWRT">"Hello my friends"</a> by Urpo
+                (CC-BY-NC-4.0)
+                <br />
+                <br />© Johnny Madigan
+              </div>
+            </div>
           </div>
         </div>
       </main>
@@ -66,7 +85,7 @@ const Home: NextPage = (props) => {
 
 const Lighting = () => (
   <>
-    <directionalLight position={[0, 0, 1]} />
+    <directionalLight position={[-1, 1, 1]} />
     <ambientLight />
   </>
 );
