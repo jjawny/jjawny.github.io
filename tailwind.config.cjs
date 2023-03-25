70c5230fc7188b640a2883d14d16da6fd149696f
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-03-25 17:13:26 +1000

    [dev] animation for fade in

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,15 @@ const config = {
       fontFamily: {
         anton: ["Anton", ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
     },
   },
   plugins: [],
