50e10751250be95582b13ff05272cb1f66095f50
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-04-03 21:00:35 +1000

    [ui] ensure fallback bg colour is black

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="bg-[url('/bg.gif')] bg-cover">
+      <body className="bg-black bg-[url('/bg.gif')] bg-cover">
         <Main />
         <NextScript />
       </body>
