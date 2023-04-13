8d3ad4ea381837978fea6dc9aad9f84a996564e0
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-04-13 22:04:12 +1000

    [lighthouse] replaced bg gif w/ mp4 for better performance

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="bg-black bg-[url('/bg.gif')] bg-cover">
+      <body className="bg-black">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="fixed top-0 left-0 h-screen w-screen object-cover"
+        >
+          <source src="bg.mp4" type="video/mp4" />
+        </video>
         <Main />
         <NextScript />
       </body>
