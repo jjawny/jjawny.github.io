42f1f1f6a4660d2b41c32c97a5a235cd12a6affe
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-12-30 20:39:03 +1000

    Removed background video

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,16 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="bg-black">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="fixed top-0 left-0 h-screen w-screen object-cover"
-        >
-          <source src="bg.mp4" type="video/mp4" />
-        </video>
+      <body style={{ backgroundColor: "cadetblue" }}>
         <Main />
         <NextScript />
       </body>
