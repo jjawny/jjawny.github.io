6d574a61c7377bbd25c64b12c6a8fa17d271bfb1
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-12-30 20:35:35 +1000

    fixed naming convention for hooks

---
+++ src/hooks/useIsInView.tsx
@@ -0,0 +1,19 @@
+import { RefObject, useEffect, useState } from "react";
+
+export const useIsInView = (ref: RefObject<Element>) => {
+  const [isInView, setIsInView] = useState<boolean>(false);
+  useEffect(() => {
+    const el = ref.current;
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry) {
+        console.log("is in view? ", entry.isIntersecting);
+        setIsInView(entry.isIntersecting);
+      }
+    });
+    if (el) observer.observe(el);
+    return () => {
+      if (el) observer.unobserve(el);
+    };
+  }, [ref]);
+  return isInView;
+};
