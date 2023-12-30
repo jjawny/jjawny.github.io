996896dfaca2ff46fd64f49f1e587a7a28429d3f
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-12-30 20:41:11 +1000

    Added dots generative three js model

---
+++ src/components/Dots.tsx
@@ -0,0 +1,44 @@
+import { pointsInner, pointsOuter } from "../utils/three.utils";
+import { Sphere, useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import _debounce from "lodash/debounce";
+import { Group, Vector3 } from "three";
+import React, { useRef } from "react";
+
+const DotsCircle = () => {
+  const ref = useRef<Group>(null);
+  const scroll = useScroll();
+
+  useFrame(({ clock }) => {
+    if (ref.current?.rotation)
+      ref.current.rotation.z = clock.getElapsedTime() * 0.05;
+
+    if (scroll && ref.current) ref.current.position.z = scroll.offset * 25; // TODO: figure this out Adjust this value to fit your needs
+  });
+
+  return (
+    <group ref={ref}>
+      {pointsInner.map((point) => (
+        <Dot key={point.idx} position={point.position} color={point.color} />
+      ))}
+      {pointsOuter.map((point) => (
+        <Dot key={point.idx} position={point.position} color={point.color} />
+      ))}
+    </group>
+  );
+};
+
+const Dot = ({ position, color }: { position: Vector3; color: string }) => {
+  return (
+    <Sphere position={position} args={[0.1, 10, 10]}>
+      <meshStandardMaterial
+        emissive={color}
+        emissiveIntensity={0.8}
+        roughness={1.0}
+        color={color}
+      />
+    </Sphere>
+  );
+};
+
+export default DotsCircle;
