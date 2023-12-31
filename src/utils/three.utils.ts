21173ffaf4b3b89b77f6d533334c29a962253346
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-12-31 11:53:59 +1000

    Updated colors to dots

--- src/utils/three.utils.ts
+++ src/utils/three.utils.ts
@@ -4,8 +4,8 @@ const DEPTH = 2;
 const MIN_RADIUS = 8;
 const MAX_RADIUS = 12;
 const NUM_POINTS = 750;
-const LEFT_COLOR = "F79A00"; // dodger blue
-const RIGHT_COLOR = "FD12D1"; // yellow
+const LEFT_COLOR = "801556";
+const RIGHT_COLOR = "ff8852";
 
 const getGradientStop = (ratio: number) => {
   ratio = Math.max(0, Math.min(ratio, 1));
