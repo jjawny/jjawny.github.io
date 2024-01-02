820b6b3dc98cac267bcd227b3f2654f0976aa4d6
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-02 19:54:02 +1000

    Added video for about section

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_VIDEO_SOURCE = "default.mp4";
 export const DEFAULT_BACKGROUND_COLOR = "white";
 export const ABOUT_SECTION_COLOR = "#fff5cc";
-export const ABOUT_SECTION_VIDEO = "jjds.mp4";
+export const ABOUT_SECTION_VIDEO = "hello-bear.mp4";
 export const PROJECTS_SOURCE = "/data/projects.json";
