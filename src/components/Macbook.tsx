a2db2450ebea4835c343e2cc26cdcbec5ac32dfe
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-02 11:41:42 +1000

    Refactored and dynamically calculate scroll/zoom stop for macbook

--- src/components/Macbook.tsx
+++ src/components/Macbook.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import MacbookKeyboard from "./Macbook.Keyboard";
-import { useGesture } from "react-use-gesture";
-import { useScroll } from "@react-three/drei";
+import { useGesture } from "@use-gesture/react";
 import { useFrame } from "@react-three/fiber";
+import { useScroll } from "@react-three/drei";
 import MacbookScreen from "./Macbook.Screen";
 import _debounce from "lodash/debounce";
 import { Group } from "three";
@@ -11,11 +11,39 @@ type MacbookProps = {
   videoSource: string;
 };
 
+const ANIMATION_DEBOUNCE_MS = 0; // debouncing animation frames causes jittery movements, but option here when testing new animations and need to improve performance
+const MIN_Z_VALUE = -50; // the min z-index for the model to travel from
+const MAX_Z_VALUE = 30; // the max z-index for the model to travel to
+const TRAVEL_SPEED = 0.4; // the speed the model travels at
+
 export function Macbook({ videoSource }: MacbookProps) {
   const groupRef = useRef<Group>(null);
 
   // ANIMATIONS
-  const debounceMs = 0; // debouncing animation frames causes jittery movements, but option here when testing new animations and need to improve performance
+  // TODO: test gyroscope orientation
+  // For gyroscope orientation
+  useEffect(() => {
+    if (window.DeviceOrientationEvent) {
+      const debouncedHandleOrientation = _debounce((event) => {
+        const { beta, gamma } = event;
+
+        if (groupRef.current) {
+          groupRef.current.rotation.x = (beta || 0) * (Math.PI / 180);
+          groupRef.current.rotation.y = (gamma || 0) * (Math.PI / 180);
+        }
+      }, ANIMATION_DEBOUNCE_MS);
+
+      window.addEventListener("deviceorientation", debouncedHandleOrientation);
+
+      return () => {
+        window.removeEventListener(
+          "deviceorientation",
+          debouncedHandleOrientation
+        );
+        debouncedHandleOrientation.cancel();
+      };
+    }
+  }, []);
 
   // For mouse movement
   const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 });
@@ -26,35 +54,14 @@ export function Macbook({ videoSource }: MacbookProps) {
         x: (event.clientX / window.innerWidth) * 2 - 1,
         y: -(event.clientY / window.innerHeight) * 2 + 1,
       });
-    }, debounceMs);
+    }, ANIMATION_DEBOUNCE_MS);
 
     window.addEventListener("mousemove", debouncedHandleMouseMove);
 
     return () => {
       window.removeEventListener("mousemove", debouncedHandleMouseMove);
+      debouncedHandleMouseMove.cancel();
     };
-  }, []);
-
-  // For gyroscope orientation
-  useEffect(() => {
-    if (window.DeviceOrientationEvent) {
-      const debouncedHandleOrientation = _debounce((event) => {
-        const { beta, gamma } = event;
-        if (groupRef.current) {
-          groupRef.current.rotation.x = (beta || 0) * (Math.PI / 180);
-          groupRef.current.rotation.y = (gamma || 0) * (Math.PI / 180);
-        }
-      }, debounceMs);
-
-      window.addEventListener("deviceorientation", debouncedHandleOrientation);
-
-      return () => {
-        window.removeEventListener(
-          "deviceorientation",
-          debouncedHandleOrientation
-        );
-      };
-    }
   }, []);
 
   // Update the model's rotation based on mouse movement
@@ -73,42 +80,39 @@ export function Macbook({ videoSource }: MacbookProps) {
   // GESTURES
   const scroll = useScroll();
 
-  const minDistance = -50;
-  const maxDistance = 30;
-  const travelSpeed = 0.4;
-
-  // % of site height (document.body.clientHeight)
-  // TODO: calc this dynamically, determine the scroll height needed to reach MAX DISTANCE at the TRAVEL SPEED
-  const maxHeightPercent = 0.1;
-
-  const bind = useGesture(
+  useGesture(
     {
       onWheel: ({ delta: [, deltaY] }) => {
-        // Zoom in model's z-axis on scroll up until maxHeightPercent
-        if (groupRef.current && scroll.offset < maxHeightPercent) {
+        // Zoom in model's z-axis on scroll up until a certain percentage of the document/site's height
+        const tenthOfViewHeight = window.innerHeight / 10;
+        const percentageOfTotalHeightToStopScrollAt =
+          tenthOfViewHeight / document.body.clientHeight;
+
+        if (
+          groupRef.current &&
+          scroll.offset < percentageOfTotalHeightToStopScrollAt
+        ) {
           const newPositionZ = Math.max(
             Math.min(
-              groupRef.current.position.z + deltaY * travelSpeed,
-              maxDistance
+              groupRef.current.position.z + deltaY * TRAVEL_SPEED,
+              MAX_Z_VALUE
             ),
-            minDistance
+            MIN_Z_VALUE
           );
           groupRef.current.position.z = newPositionZ;
         }
       },
     },
-    { domTarget: window }
+    { target: window }
   );
 
   return (
-    // @ts-ignore TODO: research this TS error more...
     <group
       dispose={null}
       ref={groupRef}
       position={[0, -1, -50]}
       scale={[0.01, 0.01, 0.01]}
       rotation={[0, Math.PI / 2, 0]}
-      {...bind()}
     >
       <MacbookScreen videoSource={videoSource} />
       <MacbookKeyboard />
