d7bbaef5138f870372d8f4753699166c694be519
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-02 16:22:32 +1000

    Implemented credits section

--- src/components/Credits.Section.tsx
+++ src/components/Credits.Section.tsx
@@ -7,7 +7,12 @@ const Credits = ({}: {}) => {
   return (
     <Html fullscreen style={{ marginTop: "300vh" }}>
       <div className={`grid h-screen w-screen items-center justify-center`}>
-        <CreditsText />
+        <div
+          className={`flex flex-col items-center justify-center justify-items-center px-[5vw]`}
+        >
+          <CreditsText />
+          <CreditsText2 />
+        </div>
       </div>
     </Html>
   );
@@ -30,17 +35,53 @@ const CreditsText = () => {
   }, [, isHovered]);
 
   return (
-    <h2
-      ref={textRef}
-      className={`rounded-md px-[2vw] font-rubik text-[1vw] font-extrabold tracking-tight sm:text-[2vw]
+    <>
+      <h2
+        ref={textRef}
+        className={`rounded-md px-[1vw] text-center font-rubik text-[3vw] font-extrabold tracking-tight sm:text-[2vw]
             ${isHovered ? "bg-white text-slate-900" : "bg-black text-white"}
             ${isInView ? "animate-fadeIn" : "invisible"}`}
-      style={{ textWrap: "nowrap" }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      {currentWord}
-    </h2>
+        style={{ textWrap: "nowrap" }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {currentWord}
+      </h2>
+    </>
+  );
+};
+
+const CreditsText2 = () => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const { currentWord, startAnimation } = useStartTextAnimation(
+    "'Macbook Pro 13 inch' by chrisgreig (CC BY)",
+    2
+  );
+  const textRef = useRef<HTMLAnchorElement>(null);
+  const isInView = useIsInView(textRef);
+
+  useEffect(() => {
+    if (isHovered) {
+      startAnimation();
+    }
+  }, [, isHovered]);
+
+  return (
+    <>
+      <a
+        ref={textRef}
+        href="https://skfb.ly/MWtY"
+        target="_blank"
+        className={`rounded-md px-[1vw] text-center font-rubik text-[1.5vw] tracking-tight sm:text-[1vw]
+            ${isHovered ? "bg-white text-slate-900" : "bg-black text-white"}
+            ${isInView ? "animate-fadeIn" : "invisible"}`}
+        style={{ textWrap: "nowrap" }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {currentWord}
+      </a>
+    </>
   );
 };
 
