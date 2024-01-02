3cab4e8af8bdd9a1cc009a2713544fac4b151370
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-02 19:09:07 +1000

    updated bg color for drawer to match color scheme

--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -44,7 +44,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col bg-black",
+        "bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col bg-slate-900",
         className
       )}
       {...props}
