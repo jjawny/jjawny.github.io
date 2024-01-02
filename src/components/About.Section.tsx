d698047c494edbf7e2a2c66aa933b61b1220e5cd
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-02 11:39:11 +1000

    Animated About section when in view

--- src/components/About.Section.tsx
+++ src/components/About.Section.tsx
@@ -1,6 +1,7 @@
+import { useStartTextAnimation } from "~/hooks/useStartTextAnimation";
 import { useEffect, useRef, useState } from "react";
+import { useIsInView } from "~/hooks/useIsInView";
 import { Html } from "@react-three/drei";
-
 import {
   Drawer,
   DrawerContent,
@@ -10,21 +11,41 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "./ui/Drawer";
-import { useStartTextAnimation } from "~/hooks/useStartTextAnimation";
 
 const About = ({
   changeBackgroundCallback,
   changeLaptopScreenCallback,
 }: {
-  changeBackgroundCallback: (newColor: string) => void;
-  changeLaptopScreenCallback: (videoUrl: string) => void;
+  changeBackgroundCallback: (newColor: string | null) => void;
+  changeLaptopScreenCallback: (videoSource: string | null) => void;
 }) => {
+  return (
+    <Html fullscreen style={{ marginTop: "100vh" }}>
+      <div className={`grid h-screen w-screen items-center justify-center`}>
+        <AboutText
+          changeBackgroundCallback={changeBackgroundCallback}
+          changeLaptopScreenCallback={changeLaptopScreenCallback}
+        />
+      </div>
+    </Html>
+  );
+};
+
+const AboutText = ({
+  changeBackgroundCallback,
+  changeLaptopScreenCallback,
+}: {
+  changeBackgroundCallback: (newColor: string | null) => void;
+  changeLaptopScreenCallback: (videoSource: string | null) => void;
+}) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
   const { currentWord, startAnimation } = useStartTextAnimation(
     "ABOUT ME",
     0.3
   );
-  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
-  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const textRef = useRef<HTMLHeadingElement>(null);
+  const isInView = useIsInView(textRef);
 
   useEffect(() => {
     if (!isDrawerOpen && isHovered) {
@@ -35,56 +56,49 @@ const About = ({
       changeBackgroundCallback("#670e3c");
       changeLaptopScreenCallback("jjds.mp4");
     } else {
-      changeBackgroundCallback("");
-      changeLaptopScreenCallback("");
+      changeBackgroundCallback(null);
+      changeLaptopScreenCallback(null);
     }
   }, [isDrawerOpen, isHovered]);
 
   return (
-    <Html fullscreen style={{ marginTop: "100vh" }}>
-      <div className="grid h-screen w-screen items-center justify-center">
-        <Drawer onOpenChange={(isOpen) => setIsDrawerOpen(isOpen)}>
-          <DrawerTrigger>
-            <h1
-              className={`rounded-md px-[2vw] font-rubik text-[8vw] font-extrabold tracking-tight 
-                ${
-                  isHovered || isDrawerOpen
-                    ? "bg-white text-slate-900"
-                    : "bg-black text-white"
-                }`}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              {currentWord}
-            </h1>
-          </DrawerTrigger>
-          <DrawerContent className="px-2">
-            <DrawerHeader>
-              <DrawerTitle className="font-rubik text-2xl text-white">
-                ABOUT ME
-              </DrawerTitle>
-              <DrawerDescription className=" py-5 text-lg text-white">
-                I&apos;m a{" "}
-                <span className="font-extrabold">
-                  full stack software developer
-                </span>
-                (.NET, React) currently working at Queensland Health on a
-                portfolio of enterprise apps.
-                <br />
-                <br />
-                As someone who <span className="font-extrabold">
-                  thrives
-                </span>{" "}
-                on mastering their stack, finding solutions to complex problems,
-                and sharing knowledge, I believe I can bring{" "}
-                <span className="font-extrabold">value</span> to any team.
-              </DrawerDescription>
-            </DrawerHeader>
-            <DrawerFooter></DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </div>
-    </Html>
+    <Drawer onOpenChange={(isOpen) => setIsDrawerOpen(isOpen)}>
+      <DrawerTrigger>
+        <h1
+          ref={textRef}
+          className={`rounded-md px-[2vw] font-rubik text-[10vw] font-extrabold tracking-tight sm:text-[8vw]
+            ${isHovered ? "bg-white text-slate-900" : "bg-black text-white"}
+            ${isInView ? "animate-fadeIn" : "invisible"}`}
+          style={{ textWrap: "nowrap" }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {currentWord}
+        </h1>
+      </DrawerTrigger>
+      <DrawerContent className="px-2">
+        <DrawerHeader>
+          <DrawerTitle className="font-rubik text-2xl text-white">
+            ABOUT ME
+          </DrawerTitle>
+          <DrawerDescription className=" py-5 text-lg text-white ">
+            I&apos;m a{" "}
+            <span className="font-extrabold">
+              full stack software developer
+            </span>
+            (.NET, React) currently working at Queensland Health on a portfolio
+            of enterprise apps.
+            <br />
+            <br />
+            As someone who <span className="font-extrabold">thrives</span> on
+            mastering their stack, finding solutions to complex problems, and
+            sharing knowledge, I believe I can bring{" "}
+            <span className="font-extrabold">value</span> to any team.
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter></DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
