f00ad7ee838bd1f93871d050102b797cd89aecb8
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-03 18:56:40 +1000

    [style] better contrast for drawer content (more solid bg)

--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -43,9 +43,13 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col bg-gradient-to-t from-white via-white/95 to-transparent",
+        "bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col",
         className
       )}
+      style={{
+        background:
+          "linear-gradient(to top, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.8) 90%, rgba(255, 255, 255, 0) 100%)",
+      }}
       {...props}
     >
       <div className="bg-muted mx-auto mt-4 h-2 w-[100px] rounded-full" />
