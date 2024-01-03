981cbfd251d809f791b90c3a631260bcc2d1a5de
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-03 10:19:59 +1000

    new hero font

--- src/components/Hero.Section.tsx
+++ src/components/Hero.Section.tsx
@@ -5,14 +5,14 @@ const Hero = () => {
   return (
     <main className="grid h-screen w-screen items-center justify-center">
       <div className="flex animate-zoomIn flex-col items-center justify-center justify-items-center">
-        <HeroText text="JOHNNY MADIGAN" />
+        <HeroText />
         <Socials />
       </div>
     </main>
   );
 };
 
-const HeroText = ({ text }: { text: string }) => {
+const HeroText = () => {
   return (
     <h1 className="cursor-default select-none text-center font-remboy text-[20vw] leading-[18vw] text-black sm:text-[11vw]">
       <span className="font-extrabold">JOHNNY</span> MADIGAN
