161846acc35609bc6460a05484c2c19e2232fc3f
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-03 11:40:06 +1000

    Simplified hero section

--- src/components/Hero.Section.tsx
+++ src/components/Hero.Section.tsx
@@ -1,22 +1,15 @@
-import { useStartTextAnimation } from "~/hooks/useStartTextAnimation";
 import Socials from "./Socials";
 
 const Hero = () => {
   return (
     <main className="grid h-screen w-screen items-center justify-center">
       <div className="flex animate-zoomIn flex-col items-center justify-center justify-items-center">
-        <HeroText />
+        <h1 className="cursor-default select-none text-center font-remboy text-[20vw] leading-[18vw] text-black sm:text-[11vw]">
+          JOHNNY MADIGAN
+        </h1>
         <Socials />
       </div>
     </main>
-  );
-};
-
-const HeroText = () => {
-  return (
-    <h1 className="cursor-default select-none text-center font-remboy text-[20vw] leading-[18vw] text-black sm:text-[11vw]">
-      <span className="font-extrabold">JOHNNY</span> MADIGAN
-    </h1>
   );
 };
 
