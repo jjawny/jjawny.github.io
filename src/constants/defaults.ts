deb1afb75d4494f8980fb67c34561081701b3acc
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-01-04 19:53:01 +1000

    [R3F] increased dots circle y travel rate

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -4,5 +4,5 @@ export const PROJECTS_SOURCE = "/data/projects.json";
 
 export const DOTS_CIRCLE_SPEED = 0.06;
 export const DOTS_CIRCLE_X_TRAVEL_RATE = 1.6;
-export const DOTS_CIRCLE_Y_TRAVEL_RATE = 6.6;
+export const DOTS_CIRCLE_Y_TRAVEL_RATE = 6.7;
 export const DOTS_CIRCLE_Z_TRAVEL_RATE = 25;
