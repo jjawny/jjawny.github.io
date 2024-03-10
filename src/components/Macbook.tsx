5d405145b2b0e249778c3d7715ed50a52164095f
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-03-10 11:09:09 +1000

    Code cleanup for Macbook component

--- src/components/Macbook.tsx
+++ src/components/Macbook.tsx
@@ -10,7 +10,7 @@ import { Group } from "three";
 const ANIMATION_DEBOUNCE_MS = 0; // debouncing animation frames causes jittery movements, but option here when testing new animations and need to improve performance
 const MIN_Z_VALUE = -50; // the min z-index for the model to travel from
 const MAX_Z_VALUE = 30; // the max z-index for the model to travel to
-const TRAVEL_SPEED = 0.4; // the speed the model travels at
+const TRAVEL_SPEED = 0.4; // the speed the model travels at when zooming
 const PERCENTAGE_TO_STOP_TRAVEL_AT = 0.1; // issues calculating this dynamically, the window.innerHeight (100vh) logs as the site height (bug with R3F?)
 
 type MacbookProps = {
@@ -22,33 +22,7 @@ const Macbook: React.FC<MacbookProps> = ({ videoSource }) => {
   const groupRef = useRef<Group>(null);
   const scroll = useScroll();
 
-  // ANIMATIONS
-  // For gyroscope orientation
-  // TODO: RnD
-  // useEffect(() => {
-  //   if (window.DeviceOrientationEvent) {
-  //     const debouncedHandleOrientation = _debounce((event) => {
-  //       const { beta, gamma } = event;
-
-  //       if (groupRef.current) {
-  //         groupRef.current.rotation.x = (beta || 0) * (Math.PI / 180);
-  //         groupRef.current.rotation.y = (gamma || 0) * (Math.PI / 180);
-  //       }
-  //     }, ANIMATION_DEBOUNCE_MS);
-
-  //     window.addEventListener("deviceorientation", debouncedHandleOrientation);
-
-  //     return () => {
-  //       window.removeEventListener(
-  //         "deviceorientation",
-  //         debouncedHandleOrientation
-  //       );
-  //       debouncedHandleOrientation.cancel();
-  //     };
-  //   }
-  // }, []);
-
-  // For mouse movement
+  // Mouse movement coords listener
   useEffect(() => {
     const debouncedHandleMouseMove = _debounce((event) => {
       setMouseCoords({
@@ -65,53 +39,37 @@ const Macbook: React.FC<MacbookProps> = ({ videoSource }) => {
     };
   }, []);
 
+  // Currently updates Macbook's rotation based on mouse coords
   useFrame(() => {
     if (groupRef.current) {
-      // Update the model's rotation based on mouse movement
       const targetRotationX = -(mouseCoords.y * (Math.PI / 8));
       const targetRotationY = mouseCoords.x * (Math.PI / 8);
 
-      groupRef.current.rotation.x +=
-        0.04 * (targetRotationX - groupRef.current.rotation.x);
-      groupRef.current.rotation.y +=
-        0.04 * (targetRotationY - groupRef.current.rotation.y);
-
-      // Update the model's position as we scroll down the page (use lerp for smooth transition)
-      // This is mainly for mobile devices to ensure the model is zoomed in after we go beyond a certain % of the page
-      const targetZ = calculateTargetZ(scroll.offset);
-
-      if (targetZ) {
-        groupRef.current.position.z = lerp(
-          groupRef.current.position.z,
-          targetZ,
-          0.1 // speed of transition
-        );
-      }
+      groupRef.current.rotation.x += 0.04 * (targetRotationX - groupRef.current.rotation.x);
+      groupRef.current.rotation.y += 0.04 * (targetRotationY - groupRef.current.rotation.y);
     }
   });
 
-  // GESTURES
   useGesture(
-    {
-      onWheel: ({ delta: [, deltaY] }) => handleZoom(deltaY),
-    },
+    { onWheel: ({ delta: [, deltaY] }) => handleZoomCallback(deltaY) },
     { target: window }
   );
 
-  const handleZoom = (value: number) => {
+  // Handles Macbook's zoom effect on scroll
+  const handleZoomCallback = (value: number) => {
     if (groupRef.current) {
-      if (scroll.offset < PERCENTAGE_TO_STOP_TRAVEL_AT) {
-        const newPositionZ = Math.max(
-          Math.min(
-            groupRef.current.position.z + value * TRAVEL_SPEED,
-            MAX_Z_VALUE
-          ),
-          MIN_Z_VALUE
-        );
-        groupRef.current.position.z = newPositionZ;
-      } else {
-        groupRef.current.position.z = MAX_Z_VALUE;
-      }
+      const newZ =
+        scroll.offset >= PERCENTAGE_TO_STOP_TRAVEL_AT
+          ? MAX_Z_VALUE
+          : Math.max(
+              Math.min(
+                groupRef.current.position.z + value * TRAVEL_SPEED,
+                MAX_Z_VALUE
+              ),
+              MIN_Z_VALUE
+            );
+
+      groupRef.current.position.z = newZ;
     }
   };
 
@@ -127,17 +85,6 @@ const Macbook: React.FC<MacbookProps> = ({ videoSource }) => {
       <MacbookKeyboard />
     </group>
   );
-};
-
-const lerp = (start: number, end: number, alpha: number) =>
-  start * (1 - alpha) + end * alpha;
-
-const calculateTargetZ = (offset: number): number | null => {
-  const tenPercentOfDocument = 0.1; // %
-
-  if (offset > tenPercentOfDocument) return MAX_Z_VALUE;
-  else if (offset < 0.01) return MIN_Z_VALUE;
-  else return null;
 };
 
 export default Macbook;
