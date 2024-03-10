3810445e1934070be16dd92796fbf523a3a4635c
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-03-10 16:26:26 +1000

    Invert drawer color to dark theme

--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -48,7 +48,7 @@ const DrawerContent = React.forwardRef<
       )}
       style={{
         background:
-          "linear-gradient(to top, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.8) 90%, rgba(255, 255, 255, 0) 100%)",
+          "linear-gradient(to top, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 80%, rgba(255, 255, 255, 0) 100%)",
       }}
       {...props}
     >
