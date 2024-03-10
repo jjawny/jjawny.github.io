64f50bca41d71ab3bb03542c2eda624e647c3ff5
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-03-10 13:01:01 +1000

    Simplified Macbook zoom animation

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -6,3 +6,5 @@ export const DOTS_CIRCLE_SPEED = 0.06;
 export const DOTS_CIRCLE_X_TRAVEL_RATE = 1.6;
 export const DOTS_CIRCLE_Y_TRAVEL_RATE = 6.7;
 export const DOTS_CIRCLE_Z_TRAVEL_RATE = 25;
+export const MACBOOK_Z_TRAVEL_RATE = 100;
+export const MACBOOK_Z_MAX = 30;
