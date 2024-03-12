082242889b2f188f6bd984db03dd88ea586899d4
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-03-12 13:37:03 +1000

    Extract grid BG style to globals CSS

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,21 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-const BODY_GRID_STYLE = {
-  backgroundColor: "#101010",
-  backgroundSize: "40px 40px",
-  backgroundImage: `linear-gradient(to right, #8080801a 1px, transparent 1px),
-                    linear-gradient(to bottom, #8080801a 1px, transparent 1px),
-                    linear-gradient(to bottom right, #8080801a 1px, transparent 1px)`,
-};
-
 export default function Document() {
   return (
     <Html>
       <Head />
-      <body
-        className="transition-colors duration-150 ease-in"
-        style={BODY_GRID_STYLE}
-      >
+      <body className="grid-bg transition-colors duration-150 ease-in">
         <Main />
         <NextScript />
       </body>
