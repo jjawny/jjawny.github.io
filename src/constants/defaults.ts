8f121c56df35f17904a617089ae0924a785bd5bf
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-08-03 19:08:43 +1000

    updated defaults

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,10 +1,9 @@
-export const DEFAULT_VIDEO_SOURCE = "default.mp4";
-export const ABOUT_SECTION_VIDEO = "about-me.mp4";
+export const DEFAULT_VIDEO_SOURCE = "me.mp4";
 export const PROJECTS_SOURCE = "/data/projects.json";
 
 export const DOTS_CIRCLE_SPEED = 0.06;
 export const DOTS_CIRCLE_X_TRAVEL_RATE = 1.6;
 export const DOTS_CIRCLE_Y_TRAVEL_RATE = 6.7;
-export const DOTS_CIRCLE_Z_TRAVEL_RATE = 25;
+export const DOTS_CIRCLE_Z_TRAVEL_RATE = 26;
 export const MACBOOK_Z_TRAVEL_RATE = 100;
 export const MACBOOK_Z_MAX = 30;
