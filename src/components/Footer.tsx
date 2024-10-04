94e9d6f2b329f7fccbb21284a17b7c89dc5272de
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-04 19:08:12 +1000

    dynamic CC year in footer and fixed styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
-const Footer = () => {
+import React from "react";
+
+const Footer: React.FC = () => {
+  const thisYear = new Date().getFullYear();
+
   return (
-    <div className="fixed bottom-0 left-0 w-full select-none px-4 py-2 text-right font-monument text-[11px] font-extrabold text-stone-600">
-      <p>Build #{process.env.NEXT_PUBLIC_BUILD_NUMBER ?? "?"}</p>
-    </div>
+    <footer className="fixed bottom-0 left-0 z-10 flex w-full select-none items-center justify-between px-4 py-2 font-monument font-extrabold text-stone-700">
+      <p className="flex-1"></p>
+      <p className="flex-1 text-center text-[0.75rem]">&copy; {thisYear} johnnymadigan</p>
+      <p className="flex-1 text-end text-[0.5rem]">Build #{process.env.NEXT_PUBLIC_BUILD_NUMBER || "n/a"}</p>
+    </footer>
   );
 };
 
