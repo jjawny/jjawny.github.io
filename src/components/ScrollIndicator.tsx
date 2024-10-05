8aedd2dda77d147883035297718951494c0cb7f3
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-05 18:20:56 +1000

    scroll indicator using mock delay hook

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
+import useMockDelay from "~/hooks/useMockDelay";
 import { useSceneContext } from "~/stores/sceneAtom";
 
 const ScrollIndicator = () => {
   const { sceneState } = useSceneContext();
   const isHide = !sceneState.isShowScrollIndicator;
+  const { isDone: isMockDelayDone } = useMockDelay();
+
+  if (!isMockDelayDone || (isMockDelayDone && isHide)) return <></>;
 
   return (
     <div
-      className={`blur-behind select-none ${isHide ? "animate-fadeOut" : "animate-[fadeIn_200ms_ease-in_forwards]"}`}
+      className={`blur-behind select-none ${isHide ? "animate-fadeOut" : "animate-[fadeIn_400ms_ease-in_forwards]"}`}
     >
       <Image
         src={"/images/mouse.svg"}
         alt="scroll or swipe to see something cool..."
-        height={50}
-        width={50}
-        className="absolute -top-1/4 left-[46%] animate-bounce grayscale invert filter"
+        height={40}
+        width={40}
+        className="absolute -top-1/4 left-[47%] animate-bounce grayscale invert filter"
       />
     </div>
   );
