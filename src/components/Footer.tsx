3965077fb522879c4527537cd1d63c5969608ca8
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-05 18:31:23 +1000

    updated footer to display environment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,10 @@ const Footer: React.FC = () => {
     <footer className="fixed bottom-0 left-0 z-10 flex w-full select-none items-center justify-between px-4 py-2 font-monument font-extrabold text-stone-700">
       <p className="flex-1"></p>
       <p className="flex-1 text-center text-[0.75rem]">&copy; {thisYear} johnnymadigan</p>
-      <p className="flex-1 text-end text-[0.5rem]">Build #{process.env.NEXT_PUBLIC_BUILD_NUMBER || "n/a"}</p>
+      <div className="flex flex-col text-end">
+        <p className="flex-1 text-[0.5rem] capitalize">Environment: {process.env.NEXT_PUBLIC_NODE_ENV || "n/a"}</p>
+        <p className="flex-1 text-[0.5rem]">Build: {process.env.NEXT_PUBLIC_BUILD_NUMBER || "n/a"}</p>
+      </div>
     </footer>
   );
 };
