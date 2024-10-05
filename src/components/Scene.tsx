9cf1dc822106e4b3a098d7cf5a38c116aa9732c2
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-05 11:25:56 +1000

    wip refactoring r3f canvas scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,14 +1,15 @@
-import { Html, ScrollControls } from "@react-three/drei";
+import { Html, OrbitControls, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useCallback, useRef, useState } from "react";
+import { Suspense, useCallback, useState } from "react";
 import DotsCircle from "~/components/DotsCircle";
-import InfoSection from "~/components/InfoSection";
+import InfoCard from "~/components/InfoCard";
 import Macbook from "~/components/Macbook";
 
 const Scene = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isAllowOrbitControls] = useState(false);
   const [isShowAbout, setIsShowAbout] = useState<boolean>(false);
   const isShowAboutCallback = useCallback((isShow: boolean) => setIsShowAbout(isShow), [setIsShowAbout]);
+
   const Lighting = () => (
     <>
       <ambientLight />
@@ -18,30 +19,43 @@ const Scene = () => {
   );
 
   return (
-    <div className="r3f-scene relative w-screen overflow-hidden">
-      <Canvas ref={canvasRef} camera={{ position: [0, 0, 35] }} className="h-screen w-screen">
-        {/* <OrbitControls /> */}
+    <div className="r3f-scene">
+      <Canvas
+        camera={{ position: [0, 0, 35] }}
+        resize={{ scroll: true, debounce: { scroll: 50, resize: 100 } }}
+        className="h-screen w-screen"
+      >
+        {isAllowOrbitControls && <OrbitControls />}
         <Lighting />
-
-        <ScrollControls pages={0.8}>
-          {/* MODELS */}
-          <Suspense fallback={null}>
-            <DotsCircle />
-          </Suspense>
-          <Suspense fallback={null}>
-            <Macbook isShowAbout={isShowAbout} />
-          </Suspense>
-
-          {/* HTML */}
-          {/* NOTE: 'zIndexRange' prop required so all z-index css applied inside works (otherwise does nothing) */}
-          {/* (resolves bug where shadcn drawer content cannot be interacted with after resize) */}
-          <Html fullscreen zIndexRange={[1, 1000]} className="feature-for-sticky-content-inside-r3f-html-overrides">
-            {/* <HeroSection /> */}
-            <InfoSection isShowAboutCallback={isShowAboutCallback} />
-          </Html>
+        <ScrollControls pages={1}>
+          <Models />
+          <R3fHtml />
         </ScrollControls>
       </Canvas>
     </div>
+  );
+};
+
+const Models: React.FC = () => {
+  return (
+    <>
+      <Suspense fallback={null}>
+        <DotsCircle />
+      </Suspense>
+      <Suspense fallback={null}>
+        <Macbook isShowAbout={false} />
+      </Suspense>
+    </>
+  );
+};
+const R3fHtml: React.FC = () => {
+  return (
+    //* NOTE: 'zIndexRange' prop required so all z-index css applied inside works (otherwise does nothing) */}
+    //* (resolves bug where shadcn drawer content cannot be interacted with after resize) */}
+    <Html fullscreen zIndexRange={[1, 1000]} className="feature-for-sticky-content-inside-r3f-html-overrides">
+      {/* <HeroSection /> */}
+      <InfoCard />
+    </Html>
   );
 };
 
