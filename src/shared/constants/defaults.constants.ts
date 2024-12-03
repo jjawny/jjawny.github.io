269e4796b65d390f6a706cad7e012c7a861e0645
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-03 21:42:46 +1000

    wip add ~ path alias and split into features

---
+++ src/shared/constants/defaults.constants.ts
@@ -0,0 +1,20 @@
+export const DEFAULT_VIDEO_SOURCE = "me.mp4";
+export const PROJECTS_SOURCE = "/data/projects.json";
+export const SCROLL_INDICATOR_THRESHOLD = 0.1;
+export const ANIMATION_THROTTLE_MS = 20;
+
+// MODEL MOVEMENT
+export const DOTS_CIRCLE_SPEED = 0.06;
+export const DOTS_CIRCLE_X_TRAVEL_RATE = 3.3;
+export const DOTS_CIRCLE_Y_TRAVEL_RATE = 9.4;
+export const DOTS_CIRCLE_Z_TRAVEL_RATE = 10;
+export const MACBOOK_Z_TRAVEL_RATE = 100;
+export const MACBOOK_Z_MAX = 30;
+
+// DOTS
+export const DEPTH = 10;
+export const MIN_RADIUS = 1;
+export const MAX_RADIUS = 12;
+export const NUM_POINTS = 500;
+export const LEFT_COLOR = "08b4f8";
+export const RIGHT_COLOR = "0502d0";
