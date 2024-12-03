f26b6c337da7f340ae1f2ceebb6e9b913d584314
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-03 21:48:05 +1000

    wip started extracting feature specific constants

---
+++ src/features/macbook/constants/macbook.constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_VIDEO_SOURCE = "me.mp4";
+export const PROJECTS_SOURCE = "/projects.json";
+export const MACBOOK_Z_TRAVEL_RATE = 100;
+export const MACBOOK_Z_MAX = 30;
--- src/shared/constants/defaults.constants.ts
+++ src/shared/constants/defaults.constants.ts
@@ -1,5 +1,3 @@
-export const DEFAULT_VIDEO_SOURCE = "me.mp4";
-export const PROJECTS_SOURCE = "/data/projects.json";
 export const SCROLL_INDICATOR_THRESHOLD = 0.1;
 export const ANIMATION_THROTTLE_MS = 20;
 
@@ -8,8 +6,6 @@ export const DOTS_CIRCLE_SPEED = 0.06;
 export const DOTS_CIRCLE_X_TRAVEL_RATE = 3.3;
 export const DOTS_CIRCLE_Y_TRAVEL_RATE = 9.4;
 export const DOTS_CIRCLE_Z_TRAVEL_RATE = 10;
-export const MACBOOK_Z_TRAVEL_RATE = 100;
-export const MACBOOK_Z_MAX = 30;
 
 // DOTS
 export const DEPTH = 10;
