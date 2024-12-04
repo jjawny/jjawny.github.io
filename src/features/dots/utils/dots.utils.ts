d6e1e2e81c350481ced8bf7dd2b9927b0061e6cc
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-04 19:15:15 +1000

    moved dots utils undef dots feature

--- src/features/dots/utils/dots.utils.ts
+++ src/features/dots/utils/dots.utils.ts
@@ -6,22 +6,19 @@ import {
   MIN_RADIUS,
   NUM_POINTS,
   RIGHT_COLOR,
-} from "../constants/defaults.constants";
+} from "~/features/dots/constants/dots.constants";
 
 // Math is hard, credits: https://youtu.be/r9IU5eJhhGo
 
 const getGradientStop = (ratio: number) => {
   ratio = Math.max(0, Math.min(ratio, 1));
 
-  const getValues = (color: string) =>
-    color.match(/.{1,2}/g)?.map((oct) => parseInt(oct, 16)) || [];
+  const getValues = (color: string) => color.match(/.{1,2}/g)?.map((oct) => parseInt(oct, 16)) || [];
 
   const c0Values = getValues(LEFT_COLOR);
   const c1Values = getValues(RIGHT_COLOR);
 
-  const ci = c0Values.map((c0, i) =>
-    Math.min(Math.round(c0 * (1 - ratio) + (c1Values[i] || 0) * ratio), 255),
-  );
+  const ci = c0Values.map((c0, i) => Math.min(Math.round(c0 * (1 - ratio) + (c1Values[i] || 0) * ratio), 255));
 
   const color = ci
     .reduce((a, v) => (a << 8) + v, 0)
@@ -44,10 +41,7 @@ const randomFromInterval = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
 };
 
-export const pointsInner = Array.from(
-  { length: NUM_POINTS },
-  (v, k) => k + 1,
-).map((num) => {
+export const pointsInner = Array.from({ length: NUM_POINTS }, (v, k) => k + 1).map((num) => {
   const randomRadius = randomFromInterval(MIN_RADIUS, MAX_RADIUS);
   const randomAngle = Math.random() * Math.PI * 2;
 
@@ -64,10 +58,7 @@ export const pointsInner = Array.from(
   };
 });
 
-export const pointsOuter = Array.from(
-  { length: NUM_POINTS / 4 },
-  (v, k) => k + 1,
-).map((num) => {
+export const pointsOuter = Array.from({ length: NUM_POINTS / 4 }, (v, k) => k + 1).map((num) => {
   const randomRadius = randomFromInterval(MIN_RADIUS / 2, MAX_RADIUS * 2);
   const angle = Math.random() * Math.PI * 2;
 
