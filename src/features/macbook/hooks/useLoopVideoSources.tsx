602742c09a85739d3249e6413ed0201e7e504b6e
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-04 18:52:50 +1000

    hook to loop through video sources

---
+++ src/features/macbook/hooks/useLoopVideoSources.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { DEFAULT_VIDEO_SOURCE, PROJECTS_SOURCE } from "~/features/macbook/constants/macbook.constants";
+import { Project } from "~/features/macbook/types/Project";
+import { useSceneStateStore } from "~/shared/hooks/useSceneStateStore";
+
+const initialVideoSource = `/videos/${DEFAULT_VIDEO_SOURCE}`;
+
+const useLoopVideoSources = () => {
+  const isShowPersonalScreen = useSceneStateStore((state) => state.sceneState.isShowPersonalScreen);
+  const [videoSource, setVideoSource] = useState<string>(initialVideoSource);
+  const [data, setData] = useState<Project[]>([]);
+  const [, setIdx] = useState<number>(-1);
+
+  useEffect(function loadData() {
+    fetch(PROJECTS_SOURCE)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  useEffect(
+    function changeVideoSource() {
+      if (isShowPersonalScreen) {
+        setVideoSource(initialVideoSource);
+        return;
+      }
+
+      const intervalId = setInterval(() => {
+        setIdx((prevIdx) => {
+          const nextIdx = (prevIdx + 1) % data.length;
+          const nextVideoSource = `/videos/${data[nextIdx]?.videoSource ?? DEFAULT_VIDEO_SOURCE}`;
+          setVideoSource(nextVideoSource);
+          return nextIdx;
+        });
+      }, 5000);
+
+      return () => clearInterval(intervalId);
+    },
+    [data, data.length, isShowPersonalScreen],
+  );
+
+  return { videoSource };
+};
+
+export default useLoopVideoSources;
