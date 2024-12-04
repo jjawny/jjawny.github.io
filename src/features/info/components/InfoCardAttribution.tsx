7765581512c4babf7e9c1d55a873967b4bc90804
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-04 20:25:57 +1000

    info card attribution component

---
+++ src/features/info/components/InfoCardAttribution.tsx
@@ -0,0 +1,13 @@
+const InfoCardAttribution: React.FC = () => {
+  return (
+    <p className="attribution">
+      <a target="_blank" rel="noopener noreferrer" href="https://skfb.ly/MWtY" className="underline">
+        <i>MacBook</i>
+      </a>{" "}
+      {/* CC Attribution (CC BY 4.0) */}
+      by chrisgreig
+    </p>
+  );
+};
+
+export default InfoCardAttribution;
