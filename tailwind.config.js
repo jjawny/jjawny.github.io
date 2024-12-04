6ada52ab26023c051786aea2c149da58fc2203d8
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-04 20:25:11 +1000

    ported over all missing tailwind animations and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,35 @@ export default {
     extend: {
       fontFamily: {
         syne: ["Syne", ...defaultTheme.fontFamily.serif],
+        default: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      animation: {
+        fadeIn: "fadeIn var(--duration, 0.25s) ease-in forwards",
+        fadeOut: "fadeOut var(--duration, 0.25s) ease-in forwards",
+        zoomIn: "zoomIn var(--duration, 0.25s) ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+        zoomIn: {
+          "0%": { transform: "scale(0) " },
+          "100%": { transform: "scale(1)" },
+        },
       },
     },
   },
