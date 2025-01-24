c21b9aa50c5c8688e0c2d296f40c1a5cfe0c8093
johnny <74356539+jjawny@users.noreply.github.com>
2025-01-24 11:45:13 +1000

    use perspective tilts

--- src/features/info/components/InfoCard.tsx
+++ src/features/info/components/InfoCard.tsx
@@ -1,26 +1,50 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import InfoCardAttribution from "~/features/info/components/InfoCardAttribution";
 import Socials from "~/features/info/components/Socials";
-import { cn } from "~/shared/utils/classname.utils";
+import { cn } from "~/features/shared/helpers/cn";
 import InfoCardName from "./InfoCardName";
 import ScrollIndicator from "./ScrollIndicator";
 
-const InfoCard: React.FC = () => {
+export default function InfoCard() {
   const [isBlurry, setIsBlurry] = useState<boolean>(false);
 
   const IntroductionFragment = () => {
-    return <p className={`hello animate-[fadeIn_200ms_ease-in_forwards] self-start`}>Hey! I&rsquo;m…</p>;
+    return (
+      <p className={cn(`hello animate-[fade-in_200ms_ease-in_forwards] self-start`, "rotate-x-10 transform-3d")}>
+        Hey! I&rsquo;m…
+      </p>
+    );
   };
 
   const TitleFragment = () => {
-    return <h2 className={`title animate-[fadeIn_600ms_ease-in_forwards]`}>Full Stack Software Developer</h2>;
+    return (
+      <h2 className={cn(`title animate-[fade-in_600ms_ease-in_forwards]`, "-rotate-x-10 transform-3d")}>
+        Full Stack Software Developer
+      </h2>
+    );
   };
 
-  const SubTitleTextFragment = () => {
+  const AttributionFragment = () => {
     return (
-      <div className="flex w-[85%] animate-[fadeIn_800ms_ease-in_forwards] justify-end pl-[15%]">
+      <div
+        className={cn("flex w-[85%] animate-[fade-in_800ms_ease-in_forwards] justify-end", "-rotate-x-10 transform-3d")}
+      >
         <div className="flex flex-col">
           <InfoCardAttribution />
+        </div>
+      </div>
+    );
+  };
+
+  const SocialsFragment = () => {
+    return (
+      <div
+        className={cn(
+          "flex w-[80%] animate-[fade-in_1000ms_ease-in_forwards] justify-end",
+          "-rotate-x-20 transform-3d",
+        )}
+      >
+        <div className="flex flex-col">
           <Socials />
         </div>
       </div>
@@ -32,19 +56,19 @@ const InfoCard: React.FC = () => {
       <div className="feature-for-sticky-content-inside-r3f-content justify-self-center">
         <div
           className={cn(
-            `transition-filter relative z-50 flex w-full select-text flex-col whitespace-nowrap text-center font-syne tracking-tight text-white duration-300 ease-in-out`,
-            isBlurry && "pointer-events-none !opacity-25 blur",
+            `transition-filter font-syne relative z-50 flex w-full flex-col text-center tracking-tight whitespace-nowrap text-white duration-300 ease-in-out select-text`,
+            isBlurry && "pointer-events-none opacity-25! blur-sm",
+            "perspective-dramatic",
           )}
         >
           <ScrollIndicator />
           <IntroductionFragment />
           <InfoCardName toggleIsBlurry={setIsBlurry} />
           <TitleFragment />
-          <SubTitleTextFragment />
+          <AttributionFragment />
+          <SocialsFragment />
         </div>
       </div>
     </div>
   );
-};
-
-export default InfoCard;
+}
