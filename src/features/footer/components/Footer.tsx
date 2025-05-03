1a986efede05fa3941373b7c68d083f916dfc4c6
johnny <74356539+jjawny@users.noreply.github.com>
2025-05-03 13:52:36 +1000

    fix: update build number display format in Footer component

--- src/features/footer/components/Footer.tsx
+++ src/features/footer/components/Footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer className="font-syne pointer-events-none fixed bottom-0 left-0 z-50 flex w-full items-center justify-between px-4 py-2 font-extrabold text-stone-700 select-none">
       <p className="flex-1"></p>
-      <p className="flex-1 text-center text-[0.75rem]">&copy; {thisYear} Johnny Madigan</p>
+      <p className="flex-1 text-center text-[0.75rem]">&copy; {thisYear}</p>
       <div className="flex flex-1 flex-col text-end">
         <p className="text-[0.5rem]">{import.meta.env.VITE_ENVIRONMENT || "Environment: n/a"}</p>
-        <p className="text-[0.5rem]">build {import.meta.env.VITE_BUILD_NUMBER || "n/a"}</p>
+        <p className="text-[0.5rem]">v{import.meta.env.VITE_BUILD_NUMBER || "n/a"}</p>
       </div>
     </footer>
   );
