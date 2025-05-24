9b7953ca3cea2b71809a856e2f5002b9718a4659
johnny <74356539+jjjjony@users.noreply.github.com>
2025-05-24 15:47:30 +1000

    split styles up for simplicity

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+
+import "./features/info/styles.css";
+import "./features/r3f/styles.css";
+import "./features/tailwind/styles.css";
+import "./styles.css";
 
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
