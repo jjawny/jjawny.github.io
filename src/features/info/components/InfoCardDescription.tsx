61b4f13526acfa6768dbb041086f4e782ebc31a4
johnny <74356539+jjjjony@users.noreply.github.com>
2025-05-24 15:47:20 +1000

    updated description

--- src/features/info/components/InfoCardDescription.tsx
+++ src/features/info/components/InfoCardDescription.tsx
@@ -1,7 +1,7 @@
 import { cn } from "~/features/shared/helpers/cn";
 
 export default function InfoCardDescription() {
-  const sharedStyles = "font-default block py-2 text-base";
+  const sharedStyles = "font-default block py-2 text-base md:text-lg";
 
   return (
     <>
@@ -9,13 +9,13 @@ export default function InfoCardDescription() {
         I&apos;m a <strong>Full Stack Software Developer</strong>
       </span>
       <span className={cn(sharedStyles)}>
-        I&apos;m experienced in translating complex business requirements into technical solutions, creating enterprise
-        web apps that automate/simplify/quantify workflows
+        I&apos;m <strong>experienced</strong> in translating complex business requirements into technical solutions,
+        creating enterprise web apps that automate/simplify/quantify workflows
       </span>
       <span className={cn(sharedStyles)}>
         {" "}
         As someone who enjoys building long-term solutions and sharing knowledge, I believe I can bring great{" "}
-        <strong>value</strong> to your team
+        <strong>value</strong> to <strong>your</strong> team
       </span>
     </>
   );
