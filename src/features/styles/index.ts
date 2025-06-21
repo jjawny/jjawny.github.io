510ed858604321db4a3f005eb9eed2e402f4cb89
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-21 22:45:39 +1000

    modularise all styles (all css files)

---
+++ src/features/styles/index.ts
@@ -0,0 +1,4 @@
+import "~/features/info/styles.css";
+import "~/features/r3f/styles.css";
+import "~/features/tailwind/styles.css";
+import "./root.css";
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-import "./features/info/styles.css";
-import "./features/r3f/styles.css";
-import "./features/tailwind/styles.css";
-import "./styles.css";
+import "~/features/styles";
 
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
