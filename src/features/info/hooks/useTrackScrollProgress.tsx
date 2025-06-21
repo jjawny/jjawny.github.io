b74401e64b7ea6416d4bfea65bf6e59f21ebeb14
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-22 08:06:05 +1000

    support detecting multiple scroll windows

--- src/features/info/hooks/useTrackScrollProgress.tsx
+++ src/features/info/hooks/useTrackScrollProgress.tsx
@@ -3,19 +3,36 @@ import { useFrame } from "@react-three/fiber";
 import { useState } from "react";
 import { MACBOOK_Z_MAX, MACBOOK_Z_TRAVEL_RATE } from "~/features/macbook/constants/macbook";
 
-export default function useTrackScrollProgress(windowMin = 0.8, windowMax = 1.0) {
-  const [scrollProgress, setScrollProgress] = useState(0);
+export type ScrollWindow = {
+  min: number;
+  max: number;
+  isScrollInside?: boolean;
+};
+
+export default function useTrackScrollProgress(scrollWindows: Record<string, ScrollWindow> = {}) {
+  const [windows, setWindows] = useState<Record<string, ScrollWindow>>(scrollWindows);
   const scroll = useScroll();
 
   useFrame(() => {
     const newZ = scroll.offset * MACBOOK_Z_TRAVEL_RATE;
     const clampedZ = newZ > MACBOOK_Z_MAX ? MACBOOK_Z_MAX : newZ;
     const progress = clampedZ / MACBOOK_Z_MAX;
-    setScrollProgress(progress);
+
+    setWindows((prev) => {
+      let hasChanged = false;
+      const updated = { ...prev };
+
+      for (const [key, window] of Object.entries(prev)) {
+        const newIsInside = progress >= window.min && progress <= window.max;
+        if (window.isScrollInside !== newIsInside) {
+          updated[key] = { ...window, isScrollInside: newIsInside };
+          hasChanged = true;
+        }
+      }
+
+      return hasChanged ? updated : prev; // return the same obj ref if no changes for perf
+    });
   });
 
-  return {
-    scrollProgress,
-    isScrolledIntoCustomWindow: scrollProgress >= windowMin && scrollProgress <= windowMax,
-  };
+  return { scrollWindows: windows };
 }
