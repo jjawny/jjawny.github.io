f2e234daa1fdf3f6ee681f026944edbe94df9b7a
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-22 00:06:54 +1000

    remove animation from name

--- src/features/info/components/InfoCardName.tsx
+++ src/features/info/components/InfoCardName.tsx
@@ -1,7 +1,7 @@
 export default function InfoCardName() {
   return (
     <main>
-      <h1 className="name relative px-[2vw] font-extrabold hover:scale-105">Johnny Madigan</h1>
+      <h1 className="name relative px-[2vw] font-extrabold">Johnny Madigan</h1>
     </main>
   );
 }
