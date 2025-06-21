20091795bd38146a33c146b1656ac3830fe931ac
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-21 23:40:29 +1000

    remove unnecessary animation styles on name

--- src/features/info/components/InfoCardName.tsx
+++ src/features/info/components/InfoCardName.tsx
@@ -1,9 +1,7 @@
 export default function InfoCardName() {
   return (
     <main>
-      <h1 className="name relative animate-[fade-in_400ms_ease-in_forwards] px-[2vw] font-extrabold duration-150 hover:scale-105">
-        Johnny Madigan
-      </h1>
+      <h1 className="name relative px-[2vw] font-extrabold hover:scale-105">Johnny Madigan</h1>
     </main>
   );
 }
