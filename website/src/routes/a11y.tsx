fca3e9b6a18e389042b2c84f9812f3183de46349
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-27 22:53:40 +1000

    wip accessability version (hidden route)

--- website/src/routes/a11y.tsx
+++ website/src/routes/a11y.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
+import InfoCardAttribution from "~/features/info/components/InfoCardAttribution";
+import InfoCardIntroduction from "~/features/info/components/InfoCardIntroduction";
+import InfoCardName from "~/features/info/components/InfoCardName";
+import InfoCardTitle from "~/features/info/components/InfoCardTitle";
+import Socials from "~/features/socials/components/Socials";
 
 export const Route = createFileRoute("/a11y")({
   component: A11y,
 });
 
 function A11y() {
-  return <div>hey</div>;
+  return (
+    <div className="grid min-h-screen place-content-center">
+      <div className="flex flex-col text-center">
+        <InfoCardIntroduction />
+        <InfoCardName />
+        <InfoCardTitle />
+        <InfoCardAttribution />
+        <Socials />
+      </div>
+    </div>
+  );
 }
