d0b2b48e02a22aaf7081038adcd513dda06ea618
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-05 22:32:04 +1000

    wrap all routes in the theme provider

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Footer from "~/features/footer/components/Footer";
 import Scene from "~/features/r3f/components/Scene";
+import { ThemeProvider } from "~/features/theme/hooks/useThemeContext";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -8,9 +9,9 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <>
+    <ThemeProvider fallbackTheme="light">
       <Scene />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
